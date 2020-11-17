a36afedb823835c41658e7f734a0b0fe3558be57
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-17 15:22:23 +0900

    active_storageの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+  
+
+  # plrivate
+
+  # def item_params
+  #   params.require(:item).permit(:image, :name, :explanation,:category_id, :condition_id, :postage_id, :arrival_date_id, :price, :user).merge(user_id: current_user.id)
+  # end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+
+
+
+
+
+
+
+
+
+  belongs_to :user
+  has_one_attached :image
 end

34bddabd9b0ee3bc2d3900afabf7079d0424efaf
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-18 18:49:40 +0900

    JSの追加およびDBに保存されているか確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_id, :arrival_date_id, :price, ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id,:arrival_date_id, :price, ).merge(user_id: current_user.id)
   end
 
 end

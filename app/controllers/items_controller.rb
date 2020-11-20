f1fccf25ea6393bd01daef343849db7fd7784e78
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-20 13:16:14 +0900

    商品一覧の表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new]
   
   def index
+    # @items = @item.includes(:user).order("created at DESC")
+    @items = Item.all.order("created_at DESC")
   end
 
   def new

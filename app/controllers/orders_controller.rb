c1c1aa47f38e0333c70ff1ac6f2c3bec51bf093e
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-25 22:29:29 +0900

    フォームオブジェクトを用いて各DBに値が保存されるように実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :find_item
+  # before_action :find_item, only:[:index]
   before_action :authenticate_user!, only:[:index]
 
   def index
@@ -9,11 +10,36 @@ class OrdersController < ApplicationController
       end
     end
     # @oreder = Order.includes(:user, :item)
+
+    @item_order = ItemOrder.new
   end
-end
+  
+  # def new
+  #   @item_order = ItemOrder.new
+  # end
+  
+  def create
+    
+    # binding.pry
+    
+    @item_order = ItemOrder.new(order_params) 
+    if @item_order.valid?
+        @item_order.save
+        redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+  
+  private
 
-private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :buillding, :phone_number).merge(item_id: params[:item_id] , user_id: current_user.id)
+    # params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :house_number, :buillding, :phone_number)
+  end
 
-def find_item
-  @item = Item.find(params[:item_id])
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
---
+++ app/models/item_order.rb
@@ -0,0 +1,20 @@
+class ItemOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :buillding, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postal_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id,         numericality: { only_integer: true}
+    validates :city,               format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :house_number
+    validates :phone_number,       numericality: { only_integer: true }
+  end
+  # validates :buillding,          format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, buillding: buillding, phone_number: phone_number, order_id: order.id )
+  end
+
+end

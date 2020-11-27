b2163614f8e2244725dd8d05a90ea23d38d32963
ryosuke <64516382+ryochaso@users.noreply.github.com>
2020-11-27 19:28:45 +0900

    商品購入機能のテストコードの実装

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -3,13 +3,15 @@ class ItemOrder
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :buillding, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :postal_code,        format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :token
+    validates :postal_code,           format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id,         numericality: { only_integer: true}
-    validates :city,               format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :city
     validates :house_number
-    validates :phone_number,       numericality: { only_integer: true }
+    validates :phone_number,          numericality: { only_integer: true }
+    validates :user_id
+    validates :item_id
   end
-  # validates :buillding,          format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
---
+++ spec/models/item_order_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+RSpec.describe ItemOrder, type: :model do
+  before do
+    @user = FactoryBot.create(:user) #フォームオブジェクトを用いているため先@user,@itemを作る必要がある
+    @item = FactoryBot.create(:item)
+    @item_order = FactoryBot.build(:item_order , user_id: @user.id, item_id: @item.id)
+    # @item_order = FactoryBot.build(:item_order)
+    sleep(1)
+  end
+
+  #以下、正常系のテストコード
+  context "商品が購入できるとき" do
+  
+    it "建物名がなくても購入できる" do
+      
+      
+      @item_order.buillding = "" 
+      expect(@item_order).to be_valid
+    end
+
+    it "全ての項目を基準を満たして入力する" do
+      expect(@item_order).to be_valid
+    end
+  end
+
+  #以下、異常系のテストコード
+  context "商品が購入できないとき" do
+    #カード情報についての項目
+    it "トークンが生成されないとき" do
+      @item_order.token = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Token can't be blank")
+    end
+
+    #郵送先に関する項目
+    it "郵便番号が空のとき" do
+      @item_order.postal_code = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it "郵便番号にハイフンがないのとき" do
+      @item_order.postal_code = "0000000"
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Postal code is invalid")
+    end
+    it "都道府県が選択されていないとき" do
+      @item_order.prefecture_id = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it "市町村が空のとき" do
+      @item_order.city = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("City can't be blank")
+    end
+    it "番地が空のとき" do
+      @item_order.house_number = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("House number can't be blank")
+    end
+    it "電話番号が空のとき" do
+      @item_order.phone_number = ""
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it "電話番号にハイフンがあるとき" do
+      @item_order.phone_number = "000-00-00000"
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone number is not a number")
+    end
+
+    #購入者および商品に関する項目
+    it "ユーザー情報が紐づいてないとき" do
+      @item_order.user_id = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("User can't be blank")
+    end
+    it "商品情報が紐づいてないとき" do
+      @item_order.item_id = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Item can't be blank")
+    end
+  end
+end
